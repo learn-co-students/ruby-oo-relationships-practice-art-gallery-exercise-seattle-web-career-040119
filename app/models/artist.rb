842a18ec115370b953bf5b9e8f783b3184ff4fd7
github username <github email address>
2019-11-04 08:49:13 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,49 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    self.all.map do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def self.most_prolific
+    self.all.max_by do |a|
+      a.paintings.length/a.years_experience
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(self, gallery, price, title)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,26 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
-    @title = title
+  def initialize(artist, gallery, price, title)
+    @artist = artist
+    @gallery = gallery
     @price = price
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.map do |painting|
+      painting.price
+    end.sum
   end
 
 end
